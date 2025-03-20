d4ce790f3c9fb84c395f1fe296260f4fdd05e531
David Ogden <DavidOgden112@gmail.com>
2025-03-20 18:00:32 +0000

    Added in Icon for lock and delete and styled

--- utils/generateCardLayout.js
+++ utils/generateCardLayout.js
@@ -15,7 +15,8 @@ export const generateCardLayout = async (recommendations) => {
     let images = await randomImage();
     return `
     ${recommendations.warning ? `<div class="text-red-500 font-semibold text-center mb-4">${recommendations.warning}</div>` : ''}
-    <button id="refresh">
+    <button id="refresh" class="bg-gradient-to-tr from-purple-900 via-indigo-800 to-purple-700 px-6 py-3 rounded-xl text-yellow-100
+    border border-yellow-200 shadow-lg transform transition-transform duration-300 hover:-translate-y-1 hover:shadow-2xl">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
 </svg>
@@ -26,8 +27,16 @@ export const generateCardLayout = async (recommendations) => {
               <h1 class="text-center font-bold">${type.toUpperCase()}</h1>
               <div id="${type}-card" class="flip-card-inner">
                   <div class="flex justify-between w-full fixed -top-6">
-                      <button id="lock-btn" type="button" class="${type} bg-emerald-500 border-2 border-solid border-emerald-500 rounded-sm p-4 hover:bg-emerald-600 hover:border-emerald-600"></button>
-                      <button id="delete-btn" type="button" class="${type} bg-red-500 border-2 border-solid border-red-500 rounded-sm p-4 hover:bg-red-700 hover:border-red-700"></button>
+                    <button id="lock-btn" type="button" class="${type} relative bg-emerald-500 border-2 border-solid border-emerald-500 rounded-sm p-4 text-center hover:bg-emerald-600 hover:border-emerald-600 text-yellow-100">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 absolute inset-x-0 mx-auto top-[40%] -translate-y-1/2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V7a4.5 4.5 0 0 0-9 0v3.5m-3 0h15a1.5 1.5 0 0 1 1.5 1.5v7a1.5 1.5 0 0 1-1.5 1.5h-15A1.5 1.5 0 0 1 3 19v-7a1.5 1.5 0 0 1 1.5-1.5z"/>
+                        </svg>
+                    </button>
+                    <button id="delete-btn" type="button" class="${type} relative bg-red-500 border-2 border-solid border-red-500 rounded-sm p-4 hover:bg-red-700 hover:border-red-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 absolute inset-x-0 mx-auto top-[40%] -translate-y-1/2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M5 6h14M6 6V5a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v1M6 6v13a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V6M8 6v13M10 6v13M12 6v13M14 6v13M16 6v13"/>
+                        </svg>
+                    </button>
                   </div>
                   <div class="${type}-card flip-card-front shadow-lg overflow-hidden border-4 border-solid border-black rounded-lg" onclick="document.querySelector('#${type}-card.flip-card-inner').classList.toggle('flipped')">
                       <img src="/Images/${images[i]}" alt="Tarot Card Back" class="w-full h-full object-cover">
